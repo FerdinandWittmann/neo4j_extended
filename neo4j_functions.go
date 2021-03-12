7db7619dc154feb6aad39428b12168aae914ae1a
u7085931 <u7085931@anu.edu.au>
2021-03-12 17:19:08 +0100

    ad

--- neo4j_functions.go
+++ neo4j_functions.go
@@ -30,15 +30,3 @@ func (r *NeoRequest) AddReturn(nodes *[]NeoNode) (err error) {
 	r.multiCypher = append(r.multiCypher, cypher)
 	return nil
 }
-
-//AddMatch adds a match statement
-func (r *NeoRequest) AddMatch(n NeoNode) (err error) {
-	cypherkey := "Match"
-	cypher, err := n.toCypher(r)
-	if err != nil {
-		return err
-	}
-	cypher = cypherkey + cypher
-	r.multiCypher = append(r.multiCypher, cypher)
-	return nil
-}
