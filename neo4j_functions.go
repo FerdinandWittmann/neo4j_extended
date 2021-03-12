99dbca011f8ff36c808ae731acb08319618e5034
u7085931 <u7085931@anu.edu.au>
2021-03-12 15:00:25 +0100

    AddReturn added

--- neo4j_functions.go
+++ neo4j_functions.go
@@ -1,5 +1,7 @@
 package neo4j_extended
 
+import "errors"
+
 //AddCreate adds a create Cypher
 func (r *NeoRequest) AddCreate(n NeoNode) (err error) {
 	cypherkey := "CREATE"
@@ -10,4 +12,20 @@ func (r *NeoRequest) AddCreate(n NeoNode) (err error) {
 	cypher = cypherkey + cypher
 	r.multiCypher = append(r.multiCypher, cypher)
 	return nil
+}
+
+//AddReturn adds a return statemnet
+func (r *NeoRequest) AddReturn(nodes *[]NeoNode) (err error) {
+	if nodes == nil {
+		return errors.New("@AddReturn: no return fields")
+	}
+	cypher := "RETURN"
+	for _, node := range *nodes {
+		if _, ok := r.names[node.Name]; !ok {
+			return errors.New("@AddReturn: node is not in query ")
+		}
+		cypher += node.Name + ", "
+	}
+	cypher = trailingComma(cypher)
+	r.multiCypher = append(r.multiCypher, cypher)
 }
